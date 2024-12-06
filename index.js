b08b79671eaf592fe4166bd8e9c23f412d8fd86e
ibnaleem <ibnaleem@outlook.com>
2024-12-06 16:11:02 +0000

    initilise bot & register slash commands

---
+++ index.js
@@ -0,0 +1,79 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { REST, Routes } from 'discord.js';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const token = process.env.CYBERCHEF_TOKEN;
+const clientId = '1314620173524930650';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+const commands = [];
+const foldersPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(foldersPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(foldersPath, file);
+    
+    try {
+        const command = await import(`file://${filePath}`);
+
+       
+        const { data, execute } = command;
+        if (data && execute) {
+            client.commands.set(data.name, command);
+            commands.push(data.toJSON());
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    } catch (error) {
+        console.error(`[ERROR] Failed to import command at ${filePath}:`, error);
+    }
+}
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    readyClient.user.setPresence({ activities: [{ name: '/help' }], status: 'online' });
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        const data = await rest.put(
+            Routes.applicationCommands(clientId),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+client.login(token);
